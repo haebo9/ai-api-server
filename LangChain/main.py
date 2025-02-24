f1dbdd54d302fce392ccdec51789632025f3fb7c
haebo <sjs68as@ajou.ac.kr>
2025-02-24 13:24:15 +0900

    Update main.py

--- LangChain/main.py
+++ LangChain/main.py
@@ -5,8 +5,5 @@ app = server.app
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+
 # uvicorn main:app --reload
-# 채팅 모델 실행
-### $ MODEL_TYPE=chat uvicorn main:app --reload
-# 번역 모델 실행
-### $ MODEL_TYPE=translate uvicorn main:app --reload
